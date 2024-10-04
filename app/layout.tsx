ff13c640d938719f2aba76ea4f707779656aa04f
Mihle Damoyi <mihle.l.damoyi@gmail.com>
2024-10-04 21:22:26 +0200

    feat: mantine setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,40 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "@/theme/mantineTheme";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "Isisele",
-  description: "An offline first education platform for farmers.",
-  manifest: "/manifest.json",
-  themeColor: "#000000",
-  viewport: "width=device-width, initial-scale=1",
+	title: "Isisele",
+	description: "An offline first education platform for farmers.",
+	manifest: "/manifest.json",
+	themeColor: "#000000",
+	viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				<MantineProvider theme={theme}>{children}</MantineProvider>
+			</body>
+		</html>
+	);
 }
---
+++ theme/mantineTheme.ts
@@ -0,0 +1,5 @@
+import { createTheme } from "@mantine/core";
+
+import "@mantine/core/styles.css";
+
+export const theme = createTheme({});
