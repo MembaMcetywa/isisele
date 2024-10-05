ef8078a4217aecb085e5163e6eb325738fc3157a
Mihle Damoyi <mihle.l.damoyi@gmail.com>
2024-10-05 11:11:27 +0200

    image s3

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-import { Paper, Stack } from "@mantine/core";
+import { Paper, Stack, Title } from "@mantine/core";
 import { ReactNode } from "react";
 
 export default function AuthLayout({
@@ -10,7 +10,10 @@ export default function AuthLayout({
 		<html>
 			<body>
 				<Stack className="w-full h-screen items-center justify-center">
-					<Paper bg="#fff" shadow="xs" m="auto" p="sm" className=" w-2/6">
+					<Paper bg="#fff" shadow="sm" m="auto" p="sm" className=" w-2/6">
+						<Title order={2} className="text-center">
+							Isisele
+						</Title>
 						{children}
 					</Paper>
 				</Stack>
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
-import withPWA from 'next-pwa'
-
+import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{ hostname: "isisele-bucket.s3.af-south-1.amazonaws.com" },
+		],
+	},
+};
 
 const pwaConfig = withPWA({
-  dest: "app",
-  disable: process.env.NODE_ENV === "development",
+	dest: "app",
+	disable: process.env.NODE_ENV === "development",
 });
 
 export default pwaConfig(nextConfig);
