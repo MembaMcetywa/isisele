ec89192e0192486125de5ae059df9389c6bc25e4
Mihle Damoyi <mihle.l.damoyi@gmail.com>
2024-10-06 07:49:44 +0200

    reactor: remove unused import

--- app/helpers/s3Bucket.ts
+++ app/helpers/s3Bucket.ts
@@ -1,6 +1,5 @@
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import axios from "axios";
 
 export async function presignedUrl(key: string) {
 	const command = new GetObjectCommand({
