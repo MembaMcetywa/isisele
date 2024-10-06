8b754aa5b4705f70ee3ba2f8bb9f96bcf80ec7bd
Mihle Damoyi <mihle.l.damoyi@gmail.com>
2024-10-06 07:48:40 +0200

    fix: any fix

--- app/helpers/s3Bucket.ts
+++ app/helpers/s3Bucket.ts
@@ -2,20 +2,20 @@ import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import axios from "axios";
 
-const s3 = new S3Client({
-	region: "af-south-1",
-	credentials: {
-		accessKeyId: process.env.AWS_ACCESS_KEY!,
-		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-	},
-});
-
 export async function presignedUrl(key: string) {
 	const command = new GetObjectCommand({
 		Bucket: process.env.AWS_BUCKET_NAME!,
 		Key: key,
 	});
 	try {
+		const s3 = new S3Client({
+			region: "af-south-1",
+			credentials: {
+				accessKeyId: process.env.AWS_ACCESS_KEY!,
+				secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+			},
+		});
+
 		return await getSignedUrl(s3, command);
 
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
